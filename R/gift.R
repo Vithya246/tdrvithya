5cf13bb827b78400a675cdce50814658f3fba8d2
Vithya Kokulathas <vithya@MacBook-Pro-de-Vithya.local>
2019-11-14 16:18:05 +0100

    add gift

--- R/gift.R
+++ R/gift.R
@@ -1,5 +1,8 @@
 #install.packages("devtools")
 library(devtools)
+usethis::browse_github_pat()
+usethis::edit_r_environ()
+
 devtools::install_github("Vithya246/tdrvithya")
 
 tdrvithya::cadeau("MPascucci")
